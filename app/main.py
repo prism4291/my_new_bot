bda1425a24a0979bc97a50362946a610719b5688
prism4291 <densikousaku7@gmail.com>
2025-07-30 20:12:34 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -15,11 +15,26 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-gemini_client = genai.Client()
+gemini_clients=[]
+for i in range(4):
+    gemini_clients.append(genai.Client(api_key=os.environ.get(f"GEMINI_API_KEY{i+1}")))
+GEMINI_API_KEY_INDEX=0
 
+
+async def get_msg(message_content):
+    gemini_response = await gemini_clients[GEMINI_API_KEY_INDEX].aio.models.generate_content(
+        model='gemini-2.5-flash',
+        contents=message_content,
+        config=types.GenerateContentConfig(
+            system_instruction='次のメッセージを、「こんにちは。ふふ。声をかけていただけると嬉しいです。」や「わあ、いただきます。」のような口調のメッセージに修正し、<answer></answer>形式で出力してください。',
+            thinking_config=types.ThinkingConfig(thinking_budget=0),
+        )
+    )
+    return gemini_response
 
 @client.event
 async def on_message(message):
+    global GEMINI_API_KEY_INDEX
     if message.author == client.user:
         return
     if message.channel.id!=1399717134162202627:
@@ -34,19 +49,16 @@ async def on_message(message):
         message_content=message.content
         message_channel=message.channel
         try:
-            gemini_response = await gemini_client.aio.models.generate_content(
-                model='gemini-2.5-flash',
-                contents=message_content,
-                config=types.GenerateContentConfig(
-                    system_instruction='次のメッセージを、「こんにちは。ふふ。声をかけていただけると嬉しいです。」や「わあ、いただきます。」のような口調のメッセージに修正し、<answer></answer>形式で出力してください。',
-                    thinking_config=types.ThinkingConfig(thinking_budget=0),
-                )
-            )
-        except genai.errors.APIError as e:
-            print("エラーです。")
-            print(message_content)
-            print(e)
-            return
+            gemini_response=await get_msg(message_content)
+        except:
+            GEMINI_API_KEY_INDEX=(GEMINI_API_KEY_INDEX+1)%4
+            try:
+                gemini_response=await get_msg(message_content)
+            except genai.errors.APIError as e:
+                print("エラーです。")
+                print(message_content)
+                print(e)
+                return
         t=gemini_response.text
         t=t.split("<answer>")[-1]
         t=t.split("</answer>")[0]
