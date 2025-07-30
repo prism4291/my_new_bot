c6ff6902a676a441a660ef91c7d1719d146ea33d
prism4291 <densikousaku7@gmail.com>
2025-07-30 19:36:47 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -33,20 +33,27 @@ async def on_message(message):
                 files_to_send.append(discord_file)
         message_content=message.content
         message_channel=message.channel
-        gemini_response = await gemini_client.aio.models.generate_content(
-            model='gemini-2.5-flash',
-            contents=message_content,
-            config=types.GenerateContentConfig(
-                system_instruction='次のメッセージを、「こんにちは。ふふ。声をかけていただけると嬉しいです。」や「わあ、いただきます。」のような口調のメッセージに修正し、<answer></answer>形式で出力してください。',
-                thinking_config=types.ThinkingConfig(thinking_budget=0),
+        try:
+            gemini_response = await gemini_client.aio.models.generate_content(
+                model='gemini-2.5-flash',
+                contents=message_content,
+                config=types.GenerateContentConfig(
+                    system_instruction='次のメッセージを、「こんにちは。ふふ。声をかけていただけると嬉しいです。」や「わあ、いただきます。」のような口調のメッセージに修正し、<answer></answer>形式で出力してください。',
+                    thinking_config=types.ThinkingConfig(thinking_budget=0),
+                )
             )
-        )
+        except genai.errors.APIError as e:
+            print("エラーです。")
+            print(message_content)
+            print(e)
+            return
         t=gemini_response.text
         t=t.split("<answer>")[-1]
         t=t.split("</answer>")[0]
         try:
             await message.delete()
         except:
+            print("削除失敗です。")
             pass
         await message_channel.send(content=t, files=files_to_send)
     
