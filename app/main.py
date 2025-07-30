b7950f192fd0a31af5e50df6526cc80948fcff38
prism4291 <densikousaku7@gmail.com>
2025-07-30 11:07:59 +0900

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,21 @@
+import discord
+import dotenv
+
+from server import server_thread
+
+dotenv.load_dotenv()
+
+TOKEN = os.environ.get("TOKEN")
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    message.channel.send(message.content)
+
+server_thread()
+client.run(TOKEN)
